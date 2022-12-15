3f5b3b6c664e1d5d2e9ce77b5f6e08e355c940d2
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2022-12-15 14:33:18 +0000

    card template from bootstrap added

--- react-notes-app/src/App.js
+++ react-notes-app/src/App.js
@@ -1,11 +1,13 @@
 
 import React from 'react'
 import Form from './components/Form'
+import Notes from './components/Notes'
 
 const App = () => {
   return (
     <div>
       <Form></Form>
+      <Notes></Notes>
     </div>
   )
 }
---
+++ react-notes-app/src/components/Notes.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Notes = () => {
+  return (
+    <>
+        <div className='card'>
+            <div className='card-body'>
+                <h5 className='card-title'>Note Title</h5>
+                <p className='card-text'>Note Desc</p>
+                <a className='btn btn-primary'>Button</a>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Notes
