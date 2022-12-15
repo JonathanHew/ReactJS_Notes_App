e8630015ad26fdb36417cd4d91f7e43ddbe52087
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2022-12-15 14:23:59 +0000

    formatted form

--- react-notes-app/src/components/Form.js
+++ react-notes-app/src/components/Form.js
@@ -3,37 +3,43 @@ import React from "react";
 const Form = () => {
   return (
     <>
-      <form>
-        <div className="mb-3">
-          <label for="title" className="form-label">
-            Title
-          </label>
-          <input type="text" className="form-control" id="title" />
-          <div id="errMsg" className="form-text"></div>
-        </div>
-        <div className="mb-3">
-          <label for="exampleInputPassword1" className="form-label">
-            Description
-          </label>
-          <textarea
-            name="desc"
-            id="desc"
-            rows="3"
-            className="form-control"
-          ></textarea>
-        </div>
-        <select id="colorInput" name="colorInput" className="form-select">
-          <option value="red" selected>
-            Red
-          </option>
-          <option value="green">Green</option>
-          <option value="blue">Blue</option>
-        </select>
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-10">
+            <form>
+              <div className="mb-3">
+                <label for="title" className="form-label">
+                  Title
+                </label>
+                <input type="text" className="form-control" id="title" />
+                <div id="errMsg" className="form-text"></div>
+              </div>
+              <div className="mb-3">
+                <label for="exampleInputPassword1" className="form-label">
+                  Description
+                </label>
+                <textarea
+                  name="desc"
+                  id="desc"
+                  rows="3"
+                  className="form-control"
+                ></textarea>
+              </div>
+              <select id="colorInput" name="colorInput" className="form-select">
+                <option value="red" selected>
+                  Red
+                </option>
+                <option value="green">Green</option>
+                <option value="blue">Blue</option>
+              </select>
 
-        <button type="submit" className="btn btn-primary mt-3">
-          Add Note
-        </button>
-      </form>
+              <button type="submit" className="btn btn-primary mt-3">
+                Add Note
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
