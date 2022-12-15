c9325db045bfc0af97e7e3f92e2ccc06d0b71dc1
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2022-12-15 14:40:31 +0000

    added delete and edit button to card

--- react-notes-app/src/App.js
+++ react-notes-app/src/App.js
@@ -10,6 +10,7 @@ const App = () => {
       <div className='container mt-5'>
         <div className='row justify-content-center'>
           <div className='col-md-10'>
+            <h1>Your Notes</h1>
             <Notes></Notes>
           </div>
         </div>
--- react-notes-app/src/components/Notes.js
+++ react-notes-app/src/components/Notes.js
@@ -7,7 +7,8 @@ const Notes = () => {
             <div className='card-body'>
                 <h5 className='card-title'>Note Title</h5>
                 <p className='card-text'>Note Desc</p>
-                <a className='btn btn-primary'>Button</a>
+                <button className='btn btn-primary'>Edit</button>
+                <button className='btn btn-danger mx-3'>Delete</button>
             </div>
         </div>
     </>
