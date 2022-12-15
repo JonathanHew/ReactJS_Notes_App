65d0971d2dd32f4f839f84f982862b0fc3aa027e
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2022-12-15 15:01:38 +0000

    existing form added into the modal

--- react-notes-app/src/components/Edit.js
+++ react-notes-app/src/components/Edit.js
@@ -23,7 +23,49 @@ const Edit = () => {
                 aria-label="Close"
               ></button>
             </div>
-            <div class="modal-body">...</div>
+            <div class="modal-body">
+              <form>
+                <div className="mb-3">
+                  <label for="title" className="form-label">
+                    Title
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="title"
+                    placeholder="Enter Note Title"
+                  />
+                  <div id="errMsg" className="form-text"></div>
+                </div>
+                <div className="mb-3">
+                  <label for="exampleInputPassword1" className="form-label">
+                    Description
+                  </label>
+                  <textarea
+                    name="desc"
+                    id="desc"
+                    rows="3"
+                    className="form-control"
+                    placeholder="Enter Note Description"
+                  ></textarea>
+                </div>
+                <select
+                  id="colorInput"
+                  name="colorInput"
+                  className="form-select"
+                >
+                  <option value="red" selected>
+                    Red
+                  </option>
+                  <option value="green">Green</option>
+                  <option value="blue">Blue</option>
+                </select>
+
+                <button type="submit" className="btn btn-primary mt-3">
+                  Add Note
+                </button>
+              </form>
+            </div>
             <div class="modal-footer">
               <button
                 type="button"
